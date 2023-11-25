48d3f783375ac45f33575a890bda7c171e95d8c6
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-11-26 00:13:51 +0200

    add class dlt

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -11,7 +11,7 @@ return (
       {stats.map(({ id, label, percentage }) => {
         
         return (
-          <li class={css.item}
+          <li className={css.item}
           key={id}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
