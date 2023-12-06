c6ee8e835e03f57a17e34b882c906edceb598ced
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-12-06 22:11:39 +0200

    add hhww

--- src/components/Friends/FriendsListItem/FriendListItem.jsx
+++ src/components/Friends/FriendsListItem/FriendListItem.jsx
@@ -1,13 +1,11 @@
-import css from './FriendListItem.module.css'
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline, id }) => {
-
-    return (
-
-        <li className={css.item} key={id}>
-            <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
-  <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-    <p className={css.name}>{name}</p>
-</li>
-    )
-}
+  return (
+    <li className={css.item}>
+      <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
